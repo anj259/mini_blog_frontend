43eca73c48bcb32115fcd58a317ed147bfb6285a
anj259 <92106605+anj259@users.noreply.github.com>
2025-12-23 09:53:58 +0530

    added route for postlist and craete post

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import './App.css'
 import Home from './home'
 import Register from './user/register'
 import Login from './user/login'
+import PostList from './posts/post_list'
+import AddPost from './posts/create_post'
+import PostByUser from './posts/postlist_byuser'
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/posts" element={<PostList />} />
+        <Route path="/addpost" element={<AddPost />} />
+        <Route path="/postbyuser/:id" element={<PostByUser />} />
       </Routes>
     </BrowserRouter>
   )
