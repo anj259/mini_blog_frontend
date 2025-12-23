0ce745b93bc73bc3a12ed2a957bb1a4dc80376e6
anj259 <92106605+anj259@users.noreply.github.com>
2025-12-23 10:25:20 +0530

    added style to register and login button

--- src/home.jsx
+++ src/home.jsx
@@ -1,16 +1,37 @@
 import { Link } from "react-router-dom";
 
+const registerbtnStyle = {
+  marginTop: "10px",
+  padding: "6px 12px",
+  backgroundColor: "#3626e9ff",
+  color: "#fff",
+  border: "none",
+  borderRadius: "5px",
+  cursor: "pointer"
+}
+
+const loginbtnStyle = {
+  marginTop: "10px",
+  padding: "6px 12px",
+  backgroundColor: "#9b26e9ff",
+  color: "#fff",
+  border: "none",
+  borderRadius: "5px",
+  cursor: "pointer",
+  marginLeft: "10px"
+}
+
 const Home = () => {
   return (
     <div style={{ textAlign: "center" }}>
       <h1>Welcome to Blog App</h1>
 
       <Link to="/register">
-        <button>Register</button>
+        <button style={registerbtnStyle}>Register</button>
       </Link>
 
       <Link to="/login">
-        <button style={{ marginLeft: "10px" }}>Login</button>
+        <button style={loginbtnStyle}>Login</button>
       </Link>
     </div>
   );
